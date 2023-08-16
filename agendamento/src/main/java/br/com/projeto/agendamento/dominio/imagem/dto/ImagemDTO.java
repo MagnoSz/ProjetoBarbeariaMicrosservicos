5df727146739615f71ef1addee4c5216b5cf75c9
Magno Souza <magno.souza@grupoitss.com.br>
2023-08-16 19:24:16 -0300

    adicionando dto de imagem

--- agendamento/src/main/java/br/com/projeto/agendamento/dominio/imagem/dto/ImagemDTO.java
+++ agendamento/src/main/java/br/com/projeto/agendamento/dominio/imagem/dto/ImagemDTO.java
@@ -1,5 +1,6 @@
 package br.com.projeto.agendamento.dominio.imagem.dto;
 
+import br.com.projeto.agendamento.dominio.imagem.entidade.Imagem;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,30 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImagemDTO {
+
+    private Long id;
+    private String nome;
+    private byte[] conteudo;
+    private String tamanho;
+    private String tipo;
+    private String dimensoes;
+
+    public Imagem toDomain() {
+        return new Imagem(id, nome, conteudo, tamanho, tipo, dimensoes);
+    }
+
+    public static ImagemDTO from(Imagem imagem) {
+        if (imagem == null) return null;
+
+        ImagemDTO imagemDTO = new ImagemDTO();
+        imagemDTO.setId(imagem.getId());
+        imagemDTO.setNome(imagem.getNome());
+        imagemDTO.setConteudo(imagem.getConteudo());
+        imagemDTO.setTamanho(imagem.getTamanho());
+        imagemDTO.setTipo(imagem.getTipo());
+        imagemDTO.setDimensoes(imagem.getDimensoes());
+
+        return imagemDTO;
+    }
+
 }
